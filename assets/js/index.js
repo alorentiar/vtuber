1cdefdb0b898f37c4192d6fbc7fc0075464c9349
alorentiar <alorentiar03@gmail.com>
2022-03-09 17:34:58 +0700

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,37 +26,54 @@ function joinvtuber() {
 }
 
 function validate() {
-  var name = document.getElementById("name");
-  var genderMale = document.getElementById("male");
-  var genderFemale = document.getElementById("female");
-  let email = document.getElementById("email");
-  var phone = document.getElementById("phone");
-  var address = document.getElementById("address");
-  var paypal = document.getElementById("paypal");
-  var skrill = document.getElementById("skrill");
+  console.log("oke");
 
-  if (name.value == "") {
+  let name = "";
+  let male = "";
+  let female = "";
+  let email = "";
+  let phone = "";
+  let address = "";
+  let paypal = "";
+  let skrill = "";
+
+  name = document.getElementById("name");
+  male = document.getElementById("maleB");
+  female = document.getElementById("femaleB");
+  email = document.getElementById("email");
+  phone = document.getElementById("phone");
+  address = document.getElementById("address");
+  paypal = document.getElementById("paypal");
+  skrill = document.getElementById("skrill");
+
+  if (name.value.length === 0) {
     alert("Name cannot be empty");
   } else if (name.value.length <= 3) {
-    window.alert("Name must be at least 3 characters");
-  } else if (!genderMale.checked && !genderFemale.checked) {
-    window.alert("Gender must be selected");
-  } else if ((email.value.length = 0)) {
-    window.alert("email cannot be empty");
-  } else if (!email.contains("@") && !email.endsWith(".com")) {
-    window.alert("Please provide a valid email that includes @ and ends with .com");
-  } else if (phone.value == "") {
-    window.alert("phone cannot be empty");
-  } else if (!phone.startsWith("+81") || phone.length != 14) {
-    window.alert("please provide a valid phone number, must starts with +81 and must be 11 digits excluding +81");
-  } else if (!address.value == "") {
-    window.alert("Address cannot be empty");
-  } else if (!address.endsWith("Street")) {
-    window.alert("Please provide Street in the end of address");
-  } else if (!paypal.checked && !skrill.checked) {
-    window.alert("Method of payment must be selected");
+    alert("Name must be at least more than 3 characters");
+  }
+  if (!male.checked && !female.checked) {
+    alert("gender must be selected");
+  }
+
+  if (email.value.length === 0) {
+    alert("email cannot be empty");
+  } else if (!email.value.includes("@") && !email.value.endsWith(".com")) {
+    alert("Please provide a valid email that includes @ and ends with .com");
+  }
+  if (phone.value.length === 0) {
+    alert("phone cannot be empty");
+  } else if (!phone.value.startsWith("+81") || phone.value.length != 14) {
+    alert("please provide a valid phone number, must starts with +81 and must be 11 digits excluding +81");
+  }
+  if (!address.value.length === 0) {
+    alert("Address cannot be empty");
+  } else if (!address.value.endsWith("Street")) {
+    alert("Please provide Street in the end of address");
+  }
+
+  if (!paypal.checked && !skrill.checked) {
+    alert("Method of payment must be selected");
   } else {
-    window.alert("Method of payment must be selected");
     localStorage.clear();
   }
 }
